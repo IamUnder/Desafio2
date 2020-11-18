e2318ad9c5e2233dc840f025e9ba6ca3c759607f
Under <jorgeolmo.I@gmail.com>
2020-11-18 09:37:31 +0100

    Actualizacion base de datos

--- Desafio2.sql
+++ Desafio2.sql
@@ -1,63 +1,60 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 16-11-2020 a las 10:27:12
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : Desafio2
+ Source Server Type    : MariaDB
+ Source Server Version : 100309
+ Source Host           : localhost:7007
+ Source Schema         : Desafio2
 
+ Target Server Type    : MariaDB
+ Target Server Version : 100309
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 18/11/2020 09:28:08
+*/
 
---
--- Base de datos: `Desafio2`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `asignacion`
---
-
+-- ----------------------------
+-- Table structure for asignacion
+-- ----------------------------
+DROP TABLE IF EXISTS `asignacion`;
 CREATE TABLE `asignacion` (
   `dni` varchar(10) NOT NULL,
   `id` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of asignacion
+-- ----------------------------
+BEGIN;
+INSERT INTO `asignacion` VALUES ('00000000X', 2);
+COMMIT;
 
---
--- Estructura de tabla para la tabla `rol`
---
-
+-- ----------------------------
+-- Table structure for rol
+-- ----------------------------
+DROP TABLE IF EXISTS `rol`;
 CREATE TABLE `rol` (
   `id` tinyint(1) NOT NULL,
   `des` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `rol`
---
+-- ----------------------------
+-- Records of rol
+-- ----------------------------
+BEGIN;
+INSERT INTO `rol` VALUES (0, 'alumno');
+INSERT INTO `rol` VALUES (1, 'profesor');
+INSERT INTO `rol` VALUES (2, 'administrador');
+COMMIT;
 
-INSERT INTO `rol` (`id`, `des`) VALUES
-(0, 'alumno'),
-(1, 'profesor'),
-(2, 'administrador');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
+-- ----------------------------
+-- Table structure for usuarios
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `dni` varchar(10) NOT NULL,
   `mail` varchar(40) NOT NULL,
@@ -67,14 +64,11 @@ CREATE TABLE `usuarios` (
   `Activado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`dni`, `mail`, `pass`, `nombre`, `apellido`, `Activado`) VALUES
-('00000000X', 'admin@admin.com', 'adminadmin1', 'nombre', 'apellido', 1);
+-- ----------------------------
+-- Records of usuarios
+-- ----------------------------
+BEGIN;
+INSERT INTO `usuarios` VALUES ('00000000X', 'admin@admin.com', 'Admin1234', 'nombre', 'apellido', 1);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
