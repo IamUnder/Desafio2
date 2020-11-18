2dad8883c0f70765e4de4cd508e2a453bbb4d2ca
Under <jorgeolmo.I@gmail.com>
2020-11-18 11:11:38 +0100

    Inicio CRUD

--- Vista/admin.php
+++ Vista/admin.php
@@ -7,7 +7,23 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CRUD Administracion</title>
+        <!-- MDB icon -->
+        <link rel="icon" href="" type="image/x-icon">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Material Design Bootstrap -->
+        <link rel="stylesheet" href="css/mdb.min.css">
+        <!-- Your custom styles (optional) -->
+        <link rel="stylesheet" href="css/style.css">
+        <!-- Codigo recaptcha v3 -->
+        <script src='https://www.google.com/recaptcha/api.js?render=6Lft9OMZAAAAAFV8-iefk3fFBnHLeb6VYp_LWaAF'>
+        </script>
     </head>
     <body>
         
