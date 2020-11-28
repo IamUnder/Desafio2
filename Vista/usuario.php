7da87630480786a6f9719a6867eabf449821d2f4
Under <jorgeolmo.I@gmail.com>
2020-11-28 17:51:22 +0100

    Diseño ventana alumno

--- Vista/usuario.php
+++ Vista/usuario.php
@@ -26,14 +26,15 @@ and open the template in the editor.
         <link rel="stylesheet" href="../css/fuentes.css">
     </head>
     <body class="rosemary">
-        
+
         <?php
         require_once '../Clases/User.php';
         require_once '../MVC/Examen.php';
         session_start();
         $user = $_SESSION['user'];
+        $allExamen = $_SESSION['allExamen'];
         ?>
-        
+
         <!--Navbar-->
         <nav class="navbar navbar-expand-lg navbar-light background-green">
 
@@ -77,11 +78,103 @@ and open the template in the editor.
 
         </nav>
         <!--/.Navbar--> 
-        
+
         <!-- Contenido -->
-        
+
+        <div class="container-fluid">
+            <div class="row">
+                <aside class="col-md-2 col-sm-2 col-lg-2 border-green pl-0 pr-0">
+                    <nav class="navbar navbar-expand-lg navbar-light my-2">
+
+
+                        <!-- Collapse button -->
+                        <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button> 
+
+                        <!-- Collapsible content -->
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                            <!-- Links -->
+                            <ul class="nav flex-column">
+                                <form action="../controlador.php" name="menu" method="POST">
+                                    <li class="nav-item">
+                                        <button class="btn btn-outline-success w-100 mt-1 border-green rounded" name="vistaEditProfile" type="submit">Editar Perfil&nbsp;<i class="fas fa-user"></i></button>
+                                    </li>
+                                    <li class="nav-item">
+                                        <button class="btn btn-outline-success w-100 mt-1 border-green rounded" name="vistaAddPreguntas" type="submit">Añadir preguntas&nbsp;<i class="fas fa-plus-circle"></i></button>
+                                    </li>
+                                    <li class="nav-item">
+                                        <button class="btn btn-outline-success w-100 mt-1 border-green rounded" name="vistaAddExamen" type="submit">Crear examen&nbsp;<i class="fas fa-file-medical"></i></button>
+                                    </li>
+                                </form>
+                            </ul>
+                            <!-- Links -->
+
+                            <!-- CTA -->
+
+                        </div>
+                    </nav> 
+                </aside>
+                <section class="col-md-10 col-lg-10 border-green">
+                    <div class="row">
+                        <div class="col-lg-10  col-md-12 text-right my-3 offset-lg-1">
+                            <p>Bienvenido: <?= $user->getNombre() ?> <i class="fas fa-user"></i></p>
+                        </div>
+                    </div>
+                    <div class="row my-4">
+                        <div class="col-6 offset-1">
+                            
+                            <div class="table-responsive">
+                                <table class="table border shadow">
+                                    <tr class="background-light-green">
+                                        <th>Titulo</th>
+                                        <th>Descripcion</th>
+                                        <th>Fecha Fin</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                    <?php
+                                    foreach ($allExamen as $v) {
+                                        if ($v->getEstado() == 1) {
+                                            ?>
+                                            
+                                    <tr>
+                                        <td>
+                                            <input type="hidden" name="id" value="<?= $v->getId() ?>" class="form-control-plaintext">
+                                            <input type="text" name="titulo" value="<?= $v->getTitulo() ?>" class="form-control-plaintext">
+                                        </td>
+                                        <td>
+                                            <input type="text" name="descripcion" value="<?= $v->getDescripcion() ?>" class="form-control-plaintext">
+                                        </td>
+                                        <td>
+                                            <input type="text" name="fecha_inicio" value="<?= $v->getFecha_Inicio() ?>" class="form-control-plaintext">
+                                        </td>
+                                        <td>
+                                            <button name="activar_examen" type="submit" class="btn-white border-0"><!-- Activar -->
+                                                <i class="fas fa-arrow-right green-text"></i>
+                                            </button>
+                                        </td>
+                                        
+                                    </tr>
+                                    <?php
+                                        }
+                                    }
+                                    ?>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-3 bg-secondary offset-1">
+                            Aqui van las notas
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+
         <!-- !Contenido -->
-        
+
         <!-- Footer -->
         <div class="container-fluid background-green">
 
@@ -94,7 +187,7 @@ and open the template in the editor.
             <!--Section: Content-->
 
         </div>
-        
+
         <!-- SCRIPTS -->
         <!-- jQuery -->
         <script type="text/javascript" src="../js/jquery.min.js"></script>
